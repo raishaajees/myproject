ae2cef0c249786738b00880e0c4a75c53f4a0ca5
raisha <raishaajees@gmail.com>
2026-08-10 19:11:41 +0530

    added task 2

---
+++ py30/task2.py
@@ -0,0 +1,17 @@
+# Take two numbers as input from the user (they will come in as strings),
+#    type-cast them to int, and print their sum, difference, product, and
+#    quotient. Use print() with multiple arguments in one line for each result.
+
+while True:
+    a=input("enter first number")
+    if a.isdigit():
+        a=int(a)
+        break
+
+while True:
+    b=input("enter second number")
+    if b.isdigit():
+        b=int(b)
+        break
+
+print(a+b,a-b,a*b,a/b)
