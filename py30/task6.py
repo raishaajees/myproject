da2a0f7f4c3037d39fc7ddabefa776f6e577e2ed
raisha <raishaajees@gmail.com>
2026-08-11 19:00:02 +0530

    added task 6

---
+++ py30/task6.py
@@ -0,0 +1,10 @@
+# # Take a number as input and print whether it is positive, negative, or zero
+#    using if / elif / else.
+
+a=int(input("enter a number:"))
+if a>0:
+    print(f"The number{a} is positive")
+elif a<0:
+    print(f"The number{a} is negative")
+else:
+    print("its zero ")
