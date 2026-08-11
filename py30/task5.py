7cf16264dc70a9373c6664af6e4041d7f51dc3de
raisha <raishaajees@gmail.com>
2026-08-11 18:45:18 +0530

    added task 5

--- py30/task5.py
+++ py30/task5.py
@@ -3,4 +3,18 @@
 #    whether it is positive AND even, whether it is negative OR zero, and
 #    whether it is divisible by both 3 and 5.
 a=int(input("enter a number: "))
-# if a>0 and a%2==0:
+if a>0 and a%2==0:
+    print("true")
+else:
+    print("false")
+if a<0 or a==0:
+    print("true")
+else:
+    print("false")
+if a%3==0 and a%5==0:
+    print("true")
+else:
+    print("false")
+
+
+
