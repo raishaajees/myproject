b0bbef87bdac855d598c04aa2a23d5ef755fc286
raisha <raishaajees@gmail.com>
2026-08-12 19:42:54 +0530

    added task 12

---
+++ py30/task12.py
@@ -0,0 +1,15 @@
+# Using a while loop, keep asking the user to enter numbers and keep adding
+#     them to a running total, stopping only when the user types "stop".
+#     Print the final total
+
+total=0
+while True:
+    a=(input("enter a num: "))
+    if a.isdigit():
+        a=int(a)
+        total=total+a
+        print(total)
+    if a=="stop":
+        break;
+
+
