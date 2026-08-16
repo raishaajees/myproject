c785c5f3b629439d035abfaa9fcbe955dd01579f
Tanishqhub <tanishqnotjwellers@gmail.com>
2026-08-16 19:22:34 +0530

    Fixed task13.py

--- py30/task13.py
+++ py30/task13.py
@@ -2,11 +2,32 @@
 #     number. Use break to stop checking as soon as you find a factor, and
 #     print an appropriate message.
 
-a=int(input("enter a number"))
-i=2
-while i<a:
-    if a%i==0:
-        print("the number is not prime")
+
+
+while True:
+    user = input("Enter a Valid whole number: ")
+    if user.isdigit():
+        user = int(user)
+        print(f"You entered {user}.")
+        break
     else:
-        print("it is a prime number")
-    break;  
+        print("Enter a numeric value only! idiot")
+
+
+
+if user < 2:
+    print(f"{user} is not a prime number")
+elif user == 2:
+    print(f"{user} is a prime number")
+elif user % 2 == 0:
+    print(f"{user} is not a prime number")
+else:
+    # root = int(user ** 0.5)
+    prime = True
+    for i in range(3,user+1,2):
+        if user % i == 0:
+            print(f"{user} is not a prime number")
+            prime = False
+
+    if prime:
+        print(f"{user} is a prime number")
