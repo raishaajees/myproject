636c95d84735c3f2d97cb78ba4f8041feec0aeed
raisha <raishaajees@gmail.com>
2026-08-16 16:27:24 +0530

    added task 14

---
+++ py30/task14.py
@@ -0,0 +1,12 @@
+# Take a sentence as input and print: its total length using len(), the
+#     same sentence in UPPERCASE, and the same sentence in lowercase.
+
+a=input("enter a sentence: ")
+print(len(a))
+b=a.upper()
+print(b)
+c=a.lower()
+print(c)
+
+
+
